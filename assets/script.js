0fd4e07b57e7577254292afe4986559ad4fc516c
Hannahdlewis <hannahdlewis07@gmail.com>
2021-10-29 19:59:31 -0400

    UPDATED JS WITH API CALL

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,7 @@ var APIKey = "cba46dfc15191b6f1973200fce8ae64b";
 // search by city name calling Api
 
 var cityName;
+
+// calling the API using the city name
+
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={APIKey};
